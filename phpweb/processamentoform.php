88ad4087d44ec05a40c7052f0e413abd8dee8921
Breno <brenosill@hotmail.com>
2023-03-06 09:14:47 -0300

    arquivos adicionados

--- phpweb/processamentoform.php
+++ phpweb/processamentoform.php
@@ -6,7 +6,9 @@
 // get enviado na url (retorna algo) / buscas
 // post enviado por segurança   / registros
 
-
-
+    $nome= $_GET['nome']; // recebendo do formularios pra ser exebido;
+    // isset para verifica se esta preenchido
 
 ?>
+
+<h1>Ola Seja muito bem <?= $nome?></h1>
